08697415338e1c8d795d30a53833f48458746ae4
Paul <paul@drurys.org>
2019-07-17 10:54:09 +0100

    fixed bug in date dropdown

--- bikes/Source/app/nav.js
+++ bikes/Source/app/nav.js
@@ -4,7 +4,7 @@
         scope.year = currentYear.year;
         scope.years = [];
 
-        var thisYear = new Date.getFullYear();
+        var thisYear = new Date().getFullYear();
 
         for (var i = 2016; i <= thisYear && i < 2026; i++) {
             scope.years.push(i.toString());
